d30c3936d3a976e3c36aecf469565bc204e6418f
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-10-06 21:07:04 +0300

    Wire googleBtn to open Google in external browser

--- QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.quicklauncher;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -24,6 +27,24 @@ public class MainActivity extends AppCompatActivity {
                 Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 startIntent.putExtra("com.example.quicklauncher.SOMETHING", "HELLO WORLD!");
                 startActivity(startIntent);
+            }
+        });
+
+        Button googleBtn = (Button)findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String google = "http://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                gotoGoogle.addCategory(Intent.CATEGORY_BROWSABLE);
+                try {
+                    startActivity(gotoGoogle);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "No browser found to open links.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
