23a11d99b0cf8ee2130c3b8715fcff8fce5663f5
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-10-06 20:04:35 +0300

    Add SecondActivity to  navigate to  from MainActivity

--- QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ QuickLauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.quicklauncher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button secondActivityBtn = (Button)findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.quicklauncher.SOMETHING", "HELLO WORLD!");
+                startActivity(startIntent);
+            }
+        });
     }
 }
---
+++ QuickLauncher/app/src/main/java/com/example/quicklauncher/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.quicklauncher;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        if (getIntent().hasExtra("com.example.quicklauncher.SOMETHING")) {
+            TextView tv = (TextView)findViewById(R.id.textView);
+            String text = getIntent().getExtras().getString("com.example.quicklauncher.SOMETHING");
+            tv.setText(text);
+        }
+    }
+}
